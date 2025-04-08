5b4bc272fc741b9c82ce2c3000d857b78539c73d
Heama3 <heamapriyas4@gmail.com>
2025-04-08 16:52:50 +0530

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,13 @@
+marks=int(input("Enter the marks:"))
+if marks>=290:
+    grade='A'
+elif marks>=280:
+    grade='B'
+elif marks>=270:
+    grade='C'
+elif marks>=260:
+    grade='D'
+else:
+    grade='F'
+
+print(f"The grade:{grade}")
