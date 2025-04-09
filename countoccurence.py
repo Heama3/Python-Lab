133b4bbe2b3adf6995032a3e2d15ff08eca96ade
Heama3 <heamapriyas4@gmail.com>
2025-04-09 18:22:17 +0530

    Add files via upload

---
+++ countoccurence.py
@@ -0,0 +1,8 @@
+def count_char_occurrences(s, char):
+    return s.count(char)
+
+# Example usage:
+s = "hello"
+char = 'l'
+result = count_char_occurrences(s, char)
+print(result)  # Output: 2
---
+++ removechar.py
@@ -0,0 +1,8 @@
+def remove_char(s, char):
+    return s.replace(char, "")
+
+# Example usage:
+s = "hello"
+char = 'l'
+result = remove_char(s, char)
+print(result)  # Output: "heo"
